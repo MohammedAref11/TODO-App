0239381fdb9011cf89badc5e92623c5b1fce24e3
MohammedAref11 <245248570+MohammedAref11@users.noreply.github.com>
2026-05-10 21:49:05 +0400

    Made the dark theme button work

--- src/main.js
+++ src/main.js
@@ -1,2 +1,29 @@
 const switchThemeBtn = document.getElementById("switchThemeBtn"); 
+let themePreference = localStorage.getItem("theme");
 
+
+
+const setThemeDark = () => { 
+    document.body.classList.remove("light")
+    document.body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+}
+
+const setThemeLight = () => {
+    document.body.classList.add("light") 
+    document.body.classList.remove("dark");
+    localStorage.setItem("theme", "light")
+}
+
+if (themePreference === "dark") { 
+    setThemeDark()
+}
+
+switchThemeBtn.addEventListener('click', () => { 
+    themePreference = localStorage.getItem("theme")
+    if (themePreference !== "dark") { 
+        setThemeDark()
+    } else if (themePreference !== "light") { 
+        setThemeLight()
+    }
+})
