93d6307fb4885af861e11a9736605198571a5bbf
MohammedAref11 <245248570+MohammedAref11@users.noreply.github.com>
2026-05-17 19:57:25 +0400

    Finished fixing the bugs and added all functions required to display all , completed and active tasks and also made clear completed tasks work

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ const checkSavedTasks = localStorage.getItem("tasks");
 const userInput = document.getElementById("userInput");
 const tasksCount = document.getElementById("tasksCounter"); 
 const tasksCon = document.getElementById("tasksCon");
+const activeBtn = document.getElementById("activeBtn"); 
+const allBtn = document.getElementById("allBtn");
+const compBtn = document.getElementById("comBtn"); 
+const clearCompBtn = document.getElementById("clearCompBtn"); 
+const btnsCon = document.getElementById("btnsCon"); 
 
 const setThemeDark = () => { 
     document.body.classList.remove("light");
@@ -38,50 +43,51 @@ switchThemeBtn.addEventListener('click', () => {
 function addTask() {
     const userInputVal = userInput.value;
         userInput.value = "";
-        saveToLocalStorage(userInputVal);
+        saveToLocalStorage(userInputVal) 
         const newDataToDis = JSON.parse(localStorage.getItem("tasks"))
         displayTasks(newDataToDis)
 }
 
-if (checkSavedTasks) { 
-    displayTasks(JSON.parse(checkSavedTasks))
-    TasksCounter()
-}
-
-function displayTasks(data) {
-    tasksCon.innerHTML = "";
-    data.forEach((e, index) => { 
-        tasksCon.innerHTML += `
-            <li class="flex justify-between px-5 py-4 border-b border-b-gray-500 cursor-grab active:cursor-grabbing">
-                  <div class="flex gap-2 items-center relative">
-                    <input type="checkbox" name="task" id="${index}" class="appearance-none cursor-pointer border border-gray-400 w-6 h-6 rounded-full checked:bg-gradient-to-br checked:from-[#57ddff] checked:to-[#c058f3] 
-                    after:absolute after:w-5 after:h-5 after:bg-[url('/images/icon-check.svg')] after:bg-no-repeat after:top-[7px] after:left-[6px] after:invisible checked:after:visible" ${e.checked ? "checked" : ""}>
-                    <label for="${index}" class="text-[var(--task-text)] select-none">${e.task}</label>
-                </div>
-                <button class="hover:cursor-pointer" id="${index}">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
-                </button>
-            </li>`;
-    })
-}
-
-function saveToLocalStorage(task) {       
-    let tasksList = localStorage.getItem("tasks");
-    if (!tasksList) { 
-        tasksList = localStorage.setItem("tasks", JSON.stringify([{task: task, checked: false}]))
+function saveToLocalStorage(userTask) { 
+    const localData = localStorage.getItem("tasks");
+    if (!localData) { 
+        localStorage.setItem("tasks", JSON.stringify([{task: userTask, checked: false}])); 
     } else { 
-        const savedTasks = JSON.parse(tasksList);
-        savedTasks.push({task: task.trim(),  checked: false})
-        localStorage.setItem("tasks", JSON.stringify(savedTasks))
+        const dataParsed = JSON.parse(localData); 
+        dataParsed.push({task: userTask, checked: false});
+        const dataStringed = JSON.stringify(dataParsed);
+        localStorage.setItem("tasks", dataStringed);
     }
 }
 
+if (checkSavedTasks) { 
+    displayTasks(JSON.parse(checkSavedTasks))
+    tasksCounter()
+}
+
+function displayTasks(data) {
+    if (data) { 
+        tasksCon.innerHTML = "";
+        data.forEach((e, index) => { 
+            tasksCon.innerHTML += `
+                <li class="flex justify-between px-5 py-4 border-b border-b-gray-500 cursor-grab active:cursor-grabbing">
+                      <div class="flex gap-2 items-center relative">
+                        <input type="checkbox" name="task" id="${index}" class="appearance-none cursor-pointer border border-gray-400 w-6 h-6 rounded-full checked:bg-gradient-to-br checked:from-[#57ddff] checked:to-[#c058f3] 
+                        after:absolute after:w-5 after:h-5 after:bg-[url('/images/icon-check.svg')] after:bg-no-repeat after:top-[7px] after:left-[6px] after:invisible checked:after:visible" ${e.checked ? "checked" : ""}>
+                        <label for="${index}" class="text-[var(--task-text)] select-none cursor-pointer">${e.task}</label>
+                    </div>
+                    <button class="hover:cursor-pointer" id="${index}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+                    </button>
+                </li>`;
+        })
+    }
+}
 
 userInput.addEventListener('keypress', (e) => {
     if (e.key === "Enter" && e.target.value.length > 0) { 
         addTask()
-        TasksCounter()
-        saveToCompleted()
+        tasksCounter()
     }
 })
 
@@ -94,41 +100,91 @@ tasksCon.addEventListener("click", (e) => {
         localStorage.setItem("tasks", stringy)
         const newDataToDis = JSON.parse(localStorage.getItem("tasks"))
         displayTasks(newDataToDis)
-        TasksCounter()
-        saveToCompleted()
+        tasksCounter()
     }
-
     const checkinput = e.target.closest("input"); 
     if (checkinput) {
-        checkinput.toggleAttribute("checked")
+        checkForCompleted()
     }
 })
 
-function TasksCounter() { 
+function tasksCounter() { 
     const dataFromLocal = JSON.parse(localStorage.getItem("tasks"))
-    tasksCount.textContent = `Items Left ${dataFromLocal.length}`
-}
-
-function saveToCompleted() { 
-    const checkLocalStorage = localStorage.getItem("completed")
-    const checkBoxEl = tasksCon.querySelectorAll("li"); 
-    const testArray = []
-    checkBoxEl.forEach(e => { 
-        const input = e.querySelector("input")
-        const labeling = e.querySelector("label"); 
-        testArray.push({task: labeling.textContent, completed: input.checked})
-    })
-    console.log(testArray)
-    if (!checkLocalStorage) { 
-        localStorage.setItem("completed", JSON.stringify([]))
-        const getLocalData = JSON.parse(localStorage.getItem("tasks"));
-
-    } else { 
- 
+    if (dataFromLocal) { 
+        tasksCount.textContent = `Items Left ${dataFromLocal.length}`
     }
 }
 
+function checkForCompleted() { 
+    const tasksContainer = document.querySelectorAll("li")
+    let newData = [];
+        tasksContainer.forEach((e) => {
+        newData.push({task: e.querySelector("label").textContent, 
+        checked: e.querySelector("input").checked})
+    })
+    localStorage.setItem("tasks", JSON.stringify(newData))
+}
+
+function displayCompleted() { 
+    const getFromLocal = localStorage.getItem("tasks");
+    const dataParsed = JSON.parse(getFromLocal); 
+    if (dataParsed) { 
+        const filterChecked = dataParsed.filter((e) => e.checked)
+        tasksCount.textContent = `Items Left ${filterChecked.length}`
+        displayTasks(filterChecked)
+    }
+}
+
+function displayActive() { 
+    const getFromLocal = localStorage.getItem("tasks");
+    const dataParsed = JSON.parse(getFromLocal); 
+    if (dataParsed) { 
+        const filterNotChecked = dataParsed.filter((e) => !e.checked); 
+        tasksCount.textContent = `Items Left ${filterNotChecked.length}`
+        displayTasks(filterNotChecked)
+    }
+}
+
+function clearCompleted() { 
+    const getFromLocal = localStorage.getItem("tasks");
+    const dataParsed = JSON.parse(getFromLocal);
+    if (dataParsed) { 
+        const filterChecked = dataParsed.filter((e) => !e.checked); 
+        localStorage.setItem("tasks", JSON.stringify(filterChecked))
+        tasksCount.textContent = `Items Left ${filterChecked.length}`
+        displayTasks(filterChecked)
+    }
+}
+
+compBtn.addEventListener("click", () => { 
+    displayCompleted()
+})
+
+allBtn.addEventListener("click", () => { 
+    const allData = localStorage.getItem("tasks"); 
+    const parsed = JSON.parse(allData)
+    displayTasks(parsed)
+    tasksCounter()
+})
 
 
+activeBtn.addEventListener("click", () => { 
+    displayActive()
+})
 
+clearCompBtn.addEventListener("click", () => { 
+    clearCompleted()
+})
 
+btnsCon.addEventListener("click", (e) => { 
+    const btn = e.target.closest("button"); 
+    const buttons = btnsCon.querySelectorAll("button"); 
+    if (btn) { 
+        buttons.forEach((e) => { 
+            if (e.classList.contains("text-blue-500")) { 
+                e.classList.remove("text-blue-500")
+            }
+        })
+        btn.classList.add("text-blue-500")
+    }
+})
