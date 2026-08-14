e8c1f44305d2a75b899251ef2aab674a482db37c
MohammedAref11 <mohammedaref.business11@gmail.com>
2026-08-14 15:52:02 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base:"/TODO-App/",
+  base:process.env.VITE_BASE_PATH || "/TODO-App",
   plugins: [
     tailwindcss(),
   ],
